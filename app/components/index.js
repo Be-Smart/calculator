2343e2ae34aa3740402a0b23933e5addba3ea796
Be-Smart <serhii.vladymyrov@gmail.com>
2016-10-11 23:29:50 +0300

    calculator logic

--- app/components/index.js
+++ app/components/index.js
@@ -5,10 +5,10 @@ import './calc.sass';
 export default class Calc {
 	constructor() {
 		this._eval = {
-			divide: (a, b) => a / b,
-			multiply: (a, b) => a * b,
-			substract: (a, b) => a - b,
-			add: (a, b) => a + b
+			'/': (a, b) => a / b,
+			'*': (a, b) => a * b,
+			'-': (a, b) => a - b,
+			'+': (a, b) => a + b
 		};
 	}
 
@@ -18,16 +18,19 @@ export default class Calc {
 
 //Convert Infix notation to Reverse Polish Notation
 	_rpn (infix) {
-		let infixArr = infix.split(/([\+\-\*\/])/);
+		let infixArr = infix.split(/([\+\-\*\/])/).filter( (x) => x !== '' );
 		let output = '';
 		let stack = [];
 		let operators = {'/': 2, '*': 2, '-': 1, '+': 1};
 
+		if (infixArr[0] === '-') output += infixArr.shift();
+
 		for (let i = 0; i < infixArr.length; i++) {
-			if ( this._isNum(infixArr[i]) ) {
-				output += `${infixArr[i]} `;
+			let sign = infixArr[i];
+			if ( this._isNum(sign) ) {
+				output += `${sign} `;
 			} else {
-				let o1 = infixArr[i];
+				let o1 = sign;
 				let o2 = stack[stack.length - 1];
 				while ("*/+-".indexOf(o2) !== -1 && (operators[o1] <= operators[o2]) ) {
 					output += `${stack.pop()} `;
@@ -47,37 +50,30 @@ export default class Calc {
 //Evaluate a Reverse Polish Notation
 	calculate (equation) {
 		let arr = this._rpn(equation).split(' ');
-		let postfix = arr.filter( (x) => x !== '' )
+		let postfix = arr.filter( (x) => x !== '' );
 		let result = [];
+		let response;
 
 		for (let i = 0; i < postfix.length; i++) {
-			if ( this._isNum(postfix[i]) ) {
-				result.push(postfix[i]);
+			let sign = postfix[i];
+
+			if ( this._isNum(sign) ) {
+				result.push(sign);
 			} else {
 				let b = +result.pop();
 				let a = +result.pop();
 
-				switch (postfix[i]) {
-					case '/':
-						result.push( this._eval.divide(a, b) );
-						break;
-					case '*':
-						result.push( this._eval.multiply(a, b) );
-						break;
-					case '-':
-						result.push( this._eval.substract(a, b) );
-						break;
-					case '+':
-						result.push( this._eval.add(a, b) );
-						break;
-				}
+				if (!this._eval[sign]) return 'Invalid equation!';
+
+				result.push( this._eval[sign](a, b) );
 			}
 		}
 
-		if (result.length > 1) return;
+		if (result.length > 1) return 'Error!';
 
-		return Math.round(result.pop() * 100) / 100;
+		response = result.pop();
+
+		return isNaN( response ) ? '0' : Math.round(response * 100) / 100;
 	}
-
 
 }
--- app/main.js
+++ app/main.js
@@ -2,6 +2,30 @@
 
 import Calc from './components';
 
-let calc = new Calc();
+const calc = new Calc();
 
-console.log( calc.calculate('25/3+31-8') );
+let btnsContainer = document.querySelector('.calc__btns');
+let display = document.querySelector('.calc__result');
+let equation = '0';
+
+function setLogic(value, dataSet) {
+	if (dataSet === 'num' || dataSet === 'minus') {
+		equation === '0' ? equation = value : equation += value;
+	} else if (value === 'ac') {
+		equation = '0';
+	} else if (value == 'ce') {
+		let length = equation.length;
+		length > 1 ? equation = equation.substr(0, length - 1) : equation = '0';
+	} else {
+		(dataSet && dataSet !== 'equal') ? equation += value : null;
+	}
+
+	dataSet === 'equal' ? equation = display.innerHTML = calc.calculate(equation) : display.innerHTML = equation;
+}
+
+btnsContainer.addEventListener('click', function(e) {
+	let val = e.target.value;
+	let data = e.target.dataset.val;
+
+	setLogic(val, data);
+});
